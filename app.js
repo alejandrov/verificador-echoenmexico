d0a133bfe0740a933b7f0fbda16f043a8c151cc3
Alejandro V. <villarroel@gmail.com>
2025-02-02 21:02:35 -0600

    2

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 // Configurar EJS como motor de plantillas
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-// Base de datos de productos de ejemplo
-const productos = {
-  "coca-cola": false,
-  tequila: true,
-  "chocolate abuelita": true,
-  iphone: false,
-  "volkswagen vocho": true,
-};
-
 // Ruta principal
 app.get("/", (req, res) => {
   res.render("index", { codigo: null, resultado: null });
 });
-// Ruta para buscar el producto
+
+// Ruta para verificar el código de barras
 app.post("/buscar", (req, res) => {
-  const codigo = req.body.codigo || null;
-  const resultado = codigo
-    ? productos[codigo] !== undefined
-      ? productos[codigo]
-      : null
-    : null;
+  const codigo = req.body.codigo || "";
+  const resultado = codigo.startsWith("750");
   res.render("index", { codigo, resultado });
 });
+
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo.`);
+  console.log(`Servidor corriendo en el puerto ${PORT}.`);
 });
